a708687aa169bd63974c56c2c187747c4694ddb8
Amitnegi1234 <amitnegi6341@gmail.com>
2023-09-22 20:10:45 +0530

    first commit

--- components/Expense.js
+++ components/Expense.js
@@ -2,25 +2,22 @@ import './Expense.css';
 
 
 
-function Expense(){
+function Expense(props){
 
-    const expenseDate=new Date(2020,4,3);
-    const Title='Car Insurance';
-    const Amount=245.67;
-    const LocationOfExpenditure='LocationOfExpenditure at insurance';
+   
     return(
         <div>
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()} </div>
+            <div>{props.date.toISOString()} </div>
             <div className='expense-item__description'>
-                <h2>{Title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price '>
-                    ${Amount}
+                    ${props.amount}
                 </div>
             </div>
         
         </div>
-     <h1>{LocationOfExpenditure}</h1>
+     
         </div>
          
     );
